4bc97a4e9c3b9f4e811178a7684b4b2ef86a2809
McGrath Coutinho <mcgrathcoutinho1@gmail.com>
2023-12-03 03:47:02 +0530

    Convert single line comments to multi-line for readability

--- test/token/ERC20/testExampleERC20.t.sol
+++ test/token/ERC20/testExampleERC20.t.sol
@@ -54,7 +54,7 @@ contract testExampleERC20 is Test {
                             Invariant Tests
     //////////////////////////////////////////////////////////////*/
 
-    // Based on my current understanding, invariant tests are tests related to a certain state that needs to be constant throughout the lifetime of a contract. They do not necessarily need to mean that the value of a state variable should remain constant but that the property of that variable with respect to some functionality is never changing. Most of these properties are covered through unit tests but as a codebase becomes complex, shifting towards the notion of invariant based testing through stateful asserts becomes crucial.
+    /* Based on my current understanding, invariant tests are tests related to a certain state that needs to be constant throughout the lifetime of a contract. They do not necessarily need to mean that the value of a state variable should remain constant but that the property of that variable with respect to some functionality is never changing. Most of these properties are covered through unit tests but as a codebase becomes complex, shifting towards the notion of invariant based testing through stateful asserts becomes crucial.*/
 
     // Tests for ERC20Metadata correctness
     function testCorrectTokenName() public {
@@ -73,7 +73,7 @@ contract testExampleERC20 is Test {
     }
 
     function testSumOfBalancesIsEqualToTotalSupply() public {
-        //Note we introduce alice and bob since there is no array tracking the key fields of mappings. Due to this, the invariant can only be tested if there is some form of onchain or offchain tracking implemented to track addresses. In this test, we look at how the totalSupply invariant persists even after minting and burning (provided only two entities - alice and bob exist)
+        /*Note we introduce alice and bob since there is no array tracking the key fields of mappings. Due to this, the invariant can only be tested if there is some form of onchain or offchain tracking implemented to track addresses. In this test, we look at how the totalSupply invariant persists even after minting and burning (provided only two entities - alice and bob exist)*/
 
         // mint tokens to alice
         instance.mintTokens(alice, 100);
